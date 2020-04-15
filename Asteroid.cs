c748211636156f8461a8fc5049ec7f6fadcaa89f
Eduardo Rodrigues <edu.crod@gmail.com>
2020-04-15 00:56:27 -0300

    adding new classes

---
+++ Asteroid.cs
@@ -0,0 +1,22 @@
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace mygame
+{
+    class Asteroid
+    {
+        public Vector2 position = new Vector2(600,300);
+        public int speed;
+        public int radius = 59;
+
+        public Asteroid(int newSpeed){
+            speed = newSpeed;
+        }
+        public void asteroidUpdate(GameTime gameTime){
+            float dt = (float)gameTime.ElapsedGameTime.TotalSeconds;
+            position.X -= speed * dt;
+        }
+    }
+}
---
+++ Controller.cs
@@ -0,0 +1,22 @@
+
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace mygame
+{
+    class Controller
+    {
+        public List<Asteroid> asteroids = new List<Asteroid>();
+        public double timer = 2D;
+        
+        public void conUpdate(GameTime gameTime){
+            timer -= gameTime.ElapsedGameTime.TotalSeconds;
+            if (timer <= 0){
+                asteroids.Add(new Asteroid(250));
+                timer = 2D;
+            }
+        }
+    }
+}
